5b71713e14959f7f623160cb3d34e90add7f4332
zacharymorel <zmorel@governmentcio.com>
2020-12-04 15:27:50 -0500

    Added and worked on test, integration and system

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,14 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10, calories: 400)
+    assert entry.save
+  end
+
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type: "Breakfast", carbohydrates: 35, proteins: 20, fats: 10)
+    assert_not entry.save
+  end
 end
--- test/system/entries_test.rb
+++ test/system/entries_test.rb
@@ -7,12 +7,12 @@ class EntriesTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit entries_url
-    assert_selector "h1", text: "Entries"
+    assert_selector "h1", text: "Foodlog - #{DateTime.now.to_s(:nice_date)}"
   end
 
   test "creating a Entry" do
     visit entries_url
-    click_on "New Entry"
+    click_on "Create new entry"
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbohydrates", with: @entry.carbohydrates
@@ -22,7 +22,6 @@ class EntriesTest < ApplicationSystemTestCase
     click_on "Create Entry"
 
     assert_text "Entry was successfully created"
-    click_on "Back"
   end
 
   test "updating a Entry" do
@@ -37,7 +36,6 @@ class EntriesTest < ApplicationSystemTestCase
     click_on "Update Entry"
 
     assert_text "Entry was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Entry" do
